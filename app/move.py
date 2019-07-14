bf90d26911cb5d6f3d8d1e087721fd919dcff5db
CtrlShift7 <Stefan.heincke@gmail.com>
2019-07-14 17:49:06 -0600

    Okay, gotta fix the suicide tendencies

--- app/move.py
+++ app/move.py
@@ -40,26 +40,34 @@ def calculate_move(board_matrix, game_state):
     # Check up
     if head["y"] - 1 < 0 or board_matrix[y-1][x] == OCCUPIED : 
         directions["up"] = -1000 # Heavily downweights moves if the square is occupied
-    else:
+    else if head["y"] - 2 > (height - 1) or board_matrix[y-2][x]== OCCUPIED
+        directions["right"] = -1000
+	else:
         directions["up"] = sum(board_matrix, head["x"], head["y"] - 1, height, game_state)
 
 
     # Check down
     if head["y"] + 1 > (height - 1) or board_matrix[y+1][x] == OCCUPIED :
         directions["down"] = -1000
-    else:
+    else if head["y"] + 2 > (height - 1) or board_matrix[y+2][x]== OCCUPIED
+        directions["right"] = -1000
+	else:
         directions["down"] = sum(board_matrix, head["x"], head["y"] + 1, height, game_state)
 
 
     # Check Left
     if head["x"] - 1 < 0 or board_matrix[y][x-1] == OCCUPIED :
         directions["left"] = -1000
-    else:
+    else if head["x"] - 2 > (height - 1) or board_matrix[y][x-2]== OCCUPIED
+        directions["right"] = -1000
+	else:
         directions["left"] = sum(board_matrix, head["x"] - 1, head["y"], height, game_state)
 
 
     # check right
     if head["x"] + 1 > (height - 1) or board_matrix[y][x+1]== OCCUPIED :
+        directions["right"] = -1000
+    else if head["x"] + 2 > (height - 1) or board_matrix[y][x+2]== OCCUPIED
         directions["right"] = -1000
     else:
         directions["right"] = sum(board_matrix, head["x"] + 1, head["y"], height, game_state)
